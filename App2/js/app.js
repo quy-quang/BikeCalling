23f6fe7499f6780ba9174f256ab4a5095eaf96d6
vrdong <caobadong19@gmail.com>
2018-11-08 20:38:36 +0700

    Gan xong app 2

--- App2/js/app.js
+++ App2/js/app.js
@@ -14,6 +14,8 @@ dataTest = [
 ]
 
 ts = 0;
+let currentMarkerLocation;
+let currentLocationName = '';
 
 window.onload = function () {
     loadCategories();
@@ -26,7 +28,7 @@ var loadCategories = function () {
         timeout: 15000
     });
 
-    // instance.get('lp?ts=' + ts)
+    // instance.get('lp?ts=' + ts) 
     //     .then(function (res) {
     //         if (res.status === 200) {
     //             ts = res.data.return_ts;
@@ -47,14 +49,81 @@ var loadCategories = function () {
     document.getElementById('list').innerHTML += html;
 }
 
-function initMap() {
-    var location = {
-        lat: -25.344,
-        lng: 131.036
-    }
-    var map = new google.maps.Map(
-        document.getElementById('map'), { zoom: 4, center: location });
 
-    var marker = new google.maps.Marker({ position: location, map: map });
-    console.log("Tao map");
+$('#myModal').on('show.bs.modal', function (e) {
+
+    //get data-id attribute of the clicked element
+    var address = $(e.relatedTarget).attr('name');
+    console.log(address);
+    getGeoCoding(address)
+        .then(location => {
+            initMap(location);
+        })
+        .catch(err => {
+            console.log(err);
+        });
+    //console.log(latLng);
+    //populate the textbox
+    //   $(e.currentTarget).find('input[name="bookId"]').val(bookId);
+});
+
+function getGeoCoding(address) {
+    return new Promise((resolve, reject) => {
+        axios.get('https://maps.googleapis.com/maps/api/geocode/json', {
+            params: {
+                address: address,
+                key: 'AIzaSyDkyzsGsAGK174yBa7KG3o5zrJDp_TpPGs'
+            }
+        }).then(function (res) {
+            if (res.status == 200) {
+                return res.data.results[0].geometry.location;
+            }
+        }).then(value => resolve(value))
+            .catch(err => reject(err));
+    })
 }
+
+function initMap(location) {
+    var map = new google.maps.Map(
+        document.getElementById('map'), { zoom: 15, center: location });
+    currentMarkerLocation = location;
+    var marker = new google.maps.Marker(
+        {
+            position: location,
+            map: map,
+            draggable: true
+        });
+
+    google.maps.event.addListener(marker, "dragend", function () {
+        var lat = marker.getPosition().lat();
+        var lng = marker.getPosition().lng();
+        currentMarkerLocation = {
+            lat: lat,
+            lng: lng
+        }
+        var geocoder = new google.maps.Geocoder;
+        var infowindow = new google.maps.InfoWindow;
+        geocoder.geocode({ 'location': currentMarkerLocation }, function (results, status) {
+            if (status === 'OK') {
+                if (results[0]) {
+                    map.setZoom(15);
+                    infowindow.setContent(results[0].formatted_address);
+                    currentLocationName = results[0].formatted_address;
+                    infowindow.open(map, marker);
+                } else {
+                    window.alert('No results found');
+                    currentLocationName = '';
+                }
+            } else {
+                window.alert('Geocoder failed due to: ' + status);
+                currentLocationName = '';
+            }
+        });
+    });
+}
+
+$('.save').on("click", function () {
+    console.log(currentMarkerLocation);
+    console.log(currentLocationName);
+
+});
