f7ebf3ce66c832c13bbe771ec0062638b618868e
QuyQuang <quyquang2421997@gmail.com>
2018-11-13 10:12:46 +0700

    done access-token

--- backend/driver/driverController.js
+++ backend/driver/driverController.js
@@ -22,46 +22,6 @@ const	WAITING	= 0,
 		MOVING	= 1,
 		DONE	= 2;
 
-//add vao database
-router.post('/', (req, res) => {
-	var userEntity = req.body;
-	console.log(userEntity.password);
-	console.log(typeof userEntity.password);
-
-	var md5_pwd = md5(userEntity.password);
-	console.log(JSON.stringify(md5_pwd))
-
-	userEntity["status"] = OFFILINE;
-	userEntity["driverId"] = shortid.generate();
-	userEntity["password"] = md5_pwd;
-	userEntity["currentLocation"] = "";
-	driverDB.get('driver').push(userEntity).write();
-
-	res.statusCode = 201;
-	res.json({
-		msg: 'received request'
-	})
-})
-
-//dang nhap
-router.post('/login', (req, res) => {
-	var loginEntity = req.body;
-	var md5_pwd = md5(loginEntity.pwd);
-	var userEntity = driverDB.get('driver').find({"username": loginEntity.user, "password": md5_pwd}).value();
-	if (userEntity != undefined){
-		var acToken = authRepo.generationAccessToken(userEntity);
-		res.json({
-			auth: true,
-			user: userEntity,
-			access_token : acToken
-		})
-	}
-	else{
-		res.json({
-			auth: false
-		})
-	}
-})
 
 router.post('/currentLocation', (req, res) => {
 	var driverId  = req.body.driverId;
